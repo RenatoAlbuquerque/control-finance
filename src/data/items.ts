b170dbe63957cade979672bcb261447b57b18ac8
Renato Albuquerque <renatoabreukz@gmail.com>
2021-10-18 18:05:35 -0300

    app

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date(2021, 9, 15), category: 'market', title: 'Feira Mensal', value: 200.12},
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300},
+    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário', value: 4500}
+];
